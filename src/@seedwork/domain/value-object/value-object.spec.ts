4b16b31897ed3391be0cbc3d6a9142e0cc1967db
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-04-24 12:52:27 -0300

    test: created valueObject

---
+++ src/@seedwork/domain/value-object/value-object.spec.ts
@@ -0,0 +1,62 @@
+import { deepFreeze } from "../utils/object";
+import { ValueObject } from "./value-object";
+
+class StubValueObject extends ValueObject {}
+
+describe("ValueObject Unit Tests", () => {
+  it("should set value", () => {
+    let vo = new StubValueObject("string value");
+    expect(vo.value).toBe("string value");
+
+    vo = new StubValueObject({ prop1: "value1" });
+    expect(vo.value).toStrictEqual({ prop1: "value1" });
+  });
+
+  it("should convert to string", () => {
+    const date = new Date();
+    let arrange = [
+      { received: "", expected: "" },
+      { received: "fake teste", expected: "fake teste" },
+      { received: 0, expected: "0" },
+      { received: 1, expected: "1" },
+      { received: 5, expected: "5" },
+      { received: true, expected: "true" },
+      { received: false, expected: "false" },
+      { received: date, expected: date.toString() },
+      {
+        received: { prop1: "value1" },
+        expected: JSON.stringify({ prop1: "value1" }),
+      },
+    ];
+    arrange.forEach((value) => {
+      const vo = new StubValueObject(value.received);
+      expect(vo + "").toBe(value.expected);
+    });
+  });
+
+  it("should be a immutable object", () => {
+    const obj = {
+      prop1: "value1",
+      deep: { prop2: "value2", prop3: new Date() },
+    };
+
+    const vo = new StubValueObject(obj);
+
+    expect(() => ((vo as any).value.prop1 = "new value")).toThrow(
+      "Cannot assign to read only property 'prop1' of object '#<Object>'"
+    );
+
+    expect(() => ((vo as any).value.deep.prop2 = "new value")).toThrow(
+      "Cannot assign to read only property 'prop2' of object '#<Object>'"
+    );
+
+    expect(vo.value.deep.prop3).toBeInstanceOf(Date);
+  });
+
+  it("immutable", () => {
+    const vo = new StubValueObject({
+      prop1: "value1",
+      nested: { prop2: "value2" },
+    });
+  });
+});
