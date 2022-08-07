c3d448d190cb91c3e1fff881bd7f0f239f54d758
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-08-07 18:28:13 -0300

    feat: entity category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,7 @@
-import { UniqueEntityId } from '../../../@seedwork/domain/value-object/unique-entity-id.vo';
-import { Entity } from '../../../@seedwork/domain/entity/entity'
-
+import { UniqueEntityId } from "../../../@seedwork/domain/value-object/unique-entity-id.vo";
+import { Entity } from "../../../@seedwork/domain/entity/entity";
+import CategoryValidatorFactory from "../validators/category.validator";
+import { EntityValidationError } from "../../../@seedwork/domain/errors/validation-error";
 
 export type CategoryProperties = {
   name: string;
@@ -9,16 +10,55 @@ export type CategoryProperties = {
   created_at?: Date;
 };
 
-export class Category extends Entity<CategoryProperties>{
+export class Category extends Entity<CategoryProperties> {
   constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    Category.validate(props);
     super(props, id);
     this.description = this.props.description;
     this.props.is_active = this.props.is_active ?? true;
     this.props.created_at = this.props.created_at ?? new Date();
   }
 
+  update(name: string, description: string) {
+    Category.validate({
+      name,
+      description,
+    });
+    this.name = name;
+    this.description = description;
+  }
+
+  static validate(props: CategoryProperties) {
+    const validator = CategoryValidatorFactory.create();
+    const isValid = validator.validate(props);
+    if (!isValid) {
+      throw new EntityValidationError(validator.errors);
+    }
+  }
+
+  // static validate(props: Omit<CategoryProperties, "created_at">) {
+  //   ValidatorRules.values(props.name, "name")
+  //     .required()
+  //     .string()
+  //     .maxLength(255);
+  //   ValidatorRules.values(props.description, "description").string();
+  //   ValidatorRules.values(props.is_active, "is_active").boolean();
+  // }
+
+  activate() {
+    this.props.is_active = true;
+  }
+
+  deactivate() {
+    this.props.is_active = false;
+  }
+
   get name() {
     return this.props.name;
+  }
+
+  private set name(value: string) {
+    this.props.name = value;
   }
 
   get description() {
