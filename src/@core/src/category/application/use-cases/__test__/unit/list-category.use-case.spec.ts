d8eb3929187788197dc50b59b0aa9f3dc9efb477
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-10-29 17:18:24 -0300

    test: list category use case

---
+++ src/@core/src/category/application/use-cases/__test__/unit/list-category.use-case.spec.ts
@@ -0,0 +1,95 @@
+import { Category } from "#category/domain";
+import { CategoryInMemoryRepository } from "#category/infra";
+import { ListCategories } from "../../list-categories.use-case";
+
+describe("ListCategoryUseCase Unit Tests", () => {
+  let useCase: ListCategories.UseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new ListCategories.UseCase(repository);
+  });
+
+  it("should returns output using empty input with categories ordered by created_at", async () => {
+    const items = [
+      new Category({ name: "test 1" }),
+      new Category({
+        name: "test 2",
+        created_at: new Date(new Date().getTime() + 100),
+      }),
+    ];
+    repository.items = items;
+
+    const output = await useCase.execute({});
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map((i) => i.toJSON()),
+      total: 2,
+      current_page: 1,
+      per_page: 15,
+      last_page: 1,
+    });
+  });
+
+  it("should returns output using pagination, sort and filter", async () => {
+    const items = [
+      new Category({ name: "a" }),
+      new Category({
+        name: "AAA",
+      }),
+      new Category({
+        name: "AaA",
+      }),
+      new Category({
+        name: "b",
+      }),
+      new Category({
+        name: "c",
+      }),
+    ];
+    repository.items = items;
+
+    let output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: "name",
+      filter: "a",
+    });
+    expect(output).toStrictEqual({
+      items: [items[1].toJSON(), items[2].toJSON()],
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 2,
+      per_page: 2,
+      sort: "name",
+      filter: "a",
+    });
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON()],
+      total: 3,
+      current_page: 2,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: "name",
+      sort_dir: "desc",
+      filter: "a",
+    });
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON(), items[2].toJSON()],
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+  });
+});
