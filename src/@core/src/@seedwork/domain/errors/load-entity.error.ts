94231943db1e82151daf62e57a7c5fb5010cbf75
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-10-29 17:22:26 -0300

    chore: error of application

---
+++ src/@core/src/@seedwork/domain/errors/load-entity.error.ts
@@ -0,0 +1,12 @@
+import { FieldsErrors } from "#seedwork/domain";
+
+export class LoadEntityError extends Error {
+  constructor(
+    public error: FieldsErrors,
+    message?: string
+  ) {
+    super(message ?? "An entity not be loaded");
+    this.name = "LoadEntityError";
+  }
+}
+export default LoadEntityError;
