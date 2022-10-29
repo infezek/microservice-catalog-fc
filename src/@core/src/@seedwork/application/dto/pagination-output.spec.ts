b08d766fd0ac560d827bf5390bd5a8b108832511
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-10-29 17:21:30 -0300

    test: search with pagination

---
+++ src/@core/src/@seedwork/application/dto/pagination-output.spec.ts
@@ -0,0 +1,33 @@
+import { SearchResult } from "../../../@seedwork/domain/repository/repository-contracts";
+import { PaginationOutputMapper } from "./pagination-output";
+
+describe("PaginationOutputMapper Unit Test", () => {
+  it("should convert a SearchResult in the output", () => {
+    const result = new SearchResult({
+      items: ["fake"] as any,
+      total: 1,
+      current_page: 1,
+      per_page: 1,
+      sort: "name",
+      sort_dir: "desc",
+      filter: "fake",
+    });
+
+    const spyToOutput = jest.spyOn(
+      PaginationOutputMapper,
+      "toOutput"
+    );
+    const output = PaginationOutputMapper.toOutput(
+      result.items,
+      result
+    );
+    expect(spyToOutput).toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      items: ["fake"],
+      total: 1,
+      current_page: 1,
+      last_page: 1,
+      per_page: 1,
+    });
+  });
+});
