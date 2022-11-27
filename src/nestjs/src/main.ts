ec773811300938cf8d668d1792b8fcef9cb5eece
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-11-27 12:15:02 -0300

    feat: apply global config

--- src/nestjs/src/main.ts
+++ src/nestjs/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { applyGlobalConfig } from './global-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  applyGlobalConfig(app);
+  await app.listen(3030);
 }
 bootstrap();
