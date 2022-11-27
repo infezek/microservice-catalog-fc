c428135897373be5e18cf1fe950db52e5d40daf1
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-11-27 11:28:09 -0300

    feat: added as initial settings in docker startup

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-if [! -f "./src/@core/.env.test"] ; then
+##### @core configs #####
+echo "#### Starting @core config ####"
+if [ ! -f "./src/@core/.env.test" ]; then
     cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
-npm install
-tail -f /dev/null
+##### nestjs configs #####
+echo "#### Starting nestjs config ####"
+if [ ! -f "./src/nestjs/envs/.env" ] ; then
+    cp ./src/nestjs/envs/example/.env.example ./src/nestjs/envs/.env
+fi
+if [ ! -f "./src/nestjs/envs/.env.test" ] ; then
+    cp ./src/nestjs/envs/example/.env.test.example ./src/nestjs/envs/.env.test
+fi
+if [ ! -f "./src/nestjs/envs/.env.e2e" ] ; then
+    cp ./src/nestjs/envs/example/.env.e2e.example ./src/nestjs/envs/.env.e2e
+fi
 
+npm install
+npm run cti:core
+npm run build:core
+
+tail -f /dev/null
 #npm run start:dev
