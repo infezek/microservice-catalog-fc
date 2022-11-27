095ac38b7a8cca2d57c0ee2c13a2aa2edb999a8d
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-11-27 12:22:56 -0300

    chore: refactor cti

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
+npm run cti create './src/@seedwork/application' -- -i '*spec.ts' -b && 
+npm run cti create './src/@seedwork/domain' -- -i '*spec.ts' -e '__test__' -e 'tests' -b && 
+npm run cti create './src/@seedwork/infra' -- -i '*spec.ts' -i 'migrator-cli.ts' -b && 
 
-npm run cti create "./src/@seedwork/application" -- -i "*spec.ts"  -b &&
-npm run cti create "./src/@seedwork/domain" -- -i "*spec.ts" -e "tests" -e "__test__" -b &&
-npm run cti create "./src/@seedworK/infra" -- -i "*spec.ts"  -b &&
-
-npm run cti create "./src/category/application" -- -i "*spec.ts"  -b &&
-npm run cti create "./src/category/domain" -- -i "*spec.ts"  -b &&
-npm run cti create "./src/category/infra" -- -i "*spec.ts"  -b
-
-
+npm run cti create './src/category/application' -- -i '*spec.ts' -b && 
+npm run cti create './src/category/domain' -- -i '*spec.ts' -b && 
+npm run cti create './src/category/infra' -- -i '*spec.ts' -b
