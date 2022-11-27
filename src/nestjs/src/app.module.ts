0fa43a41bd585a7daefe885b86d460b4f2abcc39
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-11-27 12:06:42 -0300

    feat: app route default

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
@@ -8,6 +7,6 @@ import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [ConfigModule.forRoot(), CategoriesModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
