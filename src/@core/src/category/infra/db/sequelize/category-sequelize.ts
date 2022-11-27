cba4787955675a1ad7958d8ed6b01d6bb554d228
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-11-27 11:36:07 -0300

    fix: sort in mysql

--- src/@core/src/category/infra/db/sequelize/category-sequelize.ts
+++ src/@core/src/category/infra/db/sequelize/category-sequelize.ts
@@ -6,7 +6,7 @@ import {
   Model,
 } from "sequelize-typescript";
 import { SequelizeModelFactory } from "../../../../@seedwork/infra/sequelize/sequelize-model-factory";
-import { Op } from "sequelize";
+import { literal, Op } from "sequelize";
 import {
   CategoryRepository as CategoryRepositoryContract,
   Category,
@@ -15,6 +15,7 @@ import {
   EntityValidationError,
   LoadEntityError,
   NotFoundError,
+  SortDirection,
   UniqueEntityId,
 } from "#seedwork/domain";
 
@@ -65,13 +66,18 @@ export namespace CategorySequelize {
   }
 
   export class CategoryRepository
-    implements CategoryRepositoryContract.Repository
-  {
+    implements CategoryRepositoryContract.Repository {
     sortableFields: string[] = ["name", "created_at"];
+
+    orderBy = {
+      mysql: {
+        name: (sort_dir: SortDirection) => literal(`binary name ${sort_dir}`)
+      }
+    }
 
     constructor(
       private categoryModel: typeof CategoryModel
-    ) {}
+    ) { }
 
     async insert(entity: Category): Promise<void> {
       await this.categoryModel.create(entity.toJSON());
@@ -133,8 +139,8 @@ export namespace CategorySequelize {
             },
           }),
           ...(props.sort &&
-          this.sortableFields.includes(props.sort)
-            ? { order: [[props.sort, props.sort_dir]] }
+            this.sortableFields.includes(props.sort)
+            ? { order: this.formatSort(props.sort, props.sort_dir) }
             : { order: [["created_at", "DESC"]] }),
           offset,
           limit,
@@ -151,6 +157,15 @@ export namespace CategorySequelize {
         sort_dir: props.sort_dir,
       });
     }
+
+    private formatSort(sort: string, sort_dir: SortDirection) {
+      const dialect = this.categoryModel.sequelize.getDialect();
+      if (this.orderBy[dialect] && this.orderBy[dialect][sort]) {
+        return this.orderBy[dialect][sort](sort_dir)
+      }
+      return [[sort, sort_dir]]
+    }
+
   }
 
   export class CategoryModelMapper {
